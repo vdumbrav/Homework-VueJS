294af2c748b2e0fbfa8badcfd24efff9f9274ff5
Vlad Dumbrava <vdumbrava@starlab.md>
2019-05-14 17:18:32 +0300

    update element

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+import './plugins/element.js';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <el-carousel indicator-position="outside">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.about {
+  display: flex;
+  width: 75%;
+}
+
+.el-carousel__item {
+  min-width: 100%;
+
+  h3 {
+    margin: 0;
+    font-size: 18px;
+    line-height: 300px;
+    color: #475669;
+    opacity: 0.75;
+  }
+
+  &:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  &:nth-child(2n + 1) {
+    background-color: #d3dce6;
+  }
+}
+</style>
