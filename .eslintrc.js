773f066dfeedf32c332b545a20e1f9e44395594f
Vlad Dumbrava <vdumbrava@starlab.md>
2019-05-14 16:32:07 +0300

    add config for eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,32 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/recommended", "@vue/standard"],
+  extends: [
+    'prettier',
+    'plugin:vue/strongly-recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    '@vue/standard',
+  ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': [
+      'off',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+      },
+    ],
+    'semi': ['error', 'always'],
   },
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: 'babel-eslint',
+  },
 };
