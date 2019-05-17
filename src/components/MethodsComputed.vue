7e3701ae9528c25eba954309634b42c2fa801a6e
Vlad Dumbrava <vdumbrava@starlab.md>
2019-05-17 12:39:18 +0300

    update examples

---
+++ src/components/MethodsComputed.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div class="wrapper">
+      <h2>computed: {{ fullNameComputed }}</h2>
+      <h2>methods: {{ fullNameMethod() }}</h2>
+      <el-form>
+        <el-form-item label="First Name">
+          <el-input v-model="firstName" />
+        </el-form-item>
+        <el-form-item label="Last Name">
+          <el-input v-model="lastName" />
+        </el-form-item>
+        <el-date-picker
+          @change="emitToParent($event)"
+          v-model="date"
+          :picker-options="dateRangeOptions"
+          type="date"
+          value-format="yyyy-MM-dd"
+          placeholder="Pick a day"
+        ></el-date-picker>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MethodsComputed',
+  props: {
+    msg: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+  data () {
+    return {
+      firstName: '',
+      lastName: '',
+      date: '1999-11-11',
+      dateRangeOptions: {
+        firstDayOfWeek: 1,
+      },
+    };
+  },
+  computed: {
+    fullNameComputed () {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  },
+  methods: {
+    fullNameMethod () {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    emitToParent (event) {
+      this.$emit('child-to-parent', this.date);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  max-width: 500px;
+  margin: 50px auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
+    <h2>date: {{ date }}</h2>
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MethodsComputed msg="Welcome to Your Vue.js App" @child-to-parent="onChildDate" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import MethodsComputed from '@/components/MethodsComputed.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    MethodsComputed,
+  },
+  data () {
+    return {
+      date: '',
+    };
+  },
+  methods: {
+    onChildDate (value) {
+      this.date = value;
+    },
   },
 };
 </script>
